1f51580043d5c27128e59ae6e1200c2eba9effdf
Christopher Wells <cwellsny@nycap.rr.com>
2018-12-03 11:16:20 -0500

    Solve day3 part two

--- src/three.rs
+++ src/three.rs
@@ -9,6 +9,10 @@ pub fn part_one() {
 }
 
 pub fn part_two() {
+    let claims = read_input();
+    let output = find_non_overlapping_claim(&claims).unwrap();
+
+    println!("{}", output);
 }
 
 pub fn read_input() -> Vec<Claim> {
@@ -16,9 +20,9 @@ pub fn read_input() -> Vec<Claim> {
 
     let mut claims: Vec<Claim> = Vec::new();
     for line in stdin.lock().lines() {
-        let (x1, y1, width, height) = extract_claim_info(&line.unwrap());
+        let (id, x1, y1, width, height) = extract_claim_info(&line.unwrap());
 
-        let claim = construct_claim(x1, y1, width, height);
+        let claim = construct_claim(id, x1, y1, width, height);
 
         claims.push(claim);
     }
@@ -55,14 +59,16 @@ fn count_overlap(fabric: &[[i8; 1000]; 1000]) -> i32 {
 #[derive(Debug)]
 #[derive(PartialEq)]
 pub struct Claim {
+    id: i32,
     x1: i32,
     y1: i32,
     x2: i32,
     y2: i32,
 }
 
-pub fn construct_claim(x1: i32, y1: i32, width: i32, height: i32) -> Claim {
+pub fn construct_claim(id: i32, x1: i32, y1: i32, width: i32, height: i32) -> Claim {
     Claim {
+        id,
         x1,
         y1,
         x2: x1 + width,
@@ -70,9 +76,13 @@ pub fn construct_claim(x1: i32, y1: i32, width: i32, height: i32) -> Claim {
     }
 }
 
-pub fn extract_claim_info(claim_str: &str) -> (i32, i32, i32, i32) {
+pub fn extract_claim_info(claim_str: &str) -> (i32, i32, i32, i32, i32) {
     let mut id_and_rest = claim_str.split("@");
-    id_and_rest.next();
+
+    let mut id = id_and_rest.next().unwrap().trim().to_string();
+    id.remove(0);
+
+    let id = id.parse().unwrap();
 
     let non_id = id_and_rest.next().unwrap();
 
@@ -91,7 +101,7 @@ pub fn extract_claim_info(claim_str: &str) -> (i32, i32, i32, i32) {
     let width: i32 = width_and_height.next().unwrap().parse().unwrap();
     let height: i32 = width_and_height.next().unwrap().parse().unwrap();
 
-    (x1, y1, width, height)
+    (id, x1, y1, width, height)
 }
 
 pub fn apply_claim(fabric: &mut [[i8; 1000]; 1000], claim: &Claim) {
@@ -109,15 +119,47 @@ pub fn apply_claim(fabric: &mut [[i8; 1000]; 1000], claim: &Claim) {
     }
 }
 
+pub fn find_non_overlapping_claim(claims: &Vec<Claim>) -> Option<i32> {
+    let mut fabric: [[i8; 1000]; 1000] = [[0; 1000]; 1000];
+
+    for claim in claims.iter() {
+        apply_claim(&mut fabric, claim);
+    }
+
+    for claim in claims.iter() {
+        if claim_is_non_overlapping(&fabric, claim) {
+            return Some(claim.id)
+        }
+    }
+
+    None
+}
+
+pub fn claim_is_non_overlapping(fabric: &[[i8; 1000]; 1000], claim: &Claim) -> bool {
+    for x in claim.x1 .. claim.x2 {
+        for y in claim.y1 .. claim.y2 {
+            let x = x as usize;
+            let y = y as usize;
+
+            if fabric[x][y] == 2 {
+                return false;
+            }
+        }
+    }
+
+    true
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn construct_claim_it_works_on_an_odd_case() {
-        let actual = construct_claim(2, 3, 5, 9);
+        let actual = construct_claim(1, 2, 3, 5, 9);
 
         let expected = Claim {
+            id: 1,
             x1: 2,
             y1: 3,
             x2: 7,
@@ -131,7 +173,7 @@ mod tests {
     fn extract_claim_info_it_works_on_an_odd_case() {
         let claim_str = "#1 @ 704,926: 5x4";
 
-        let expected = (704, 926, 5, 4);
+        let expected = (1, 704, 926, 5, 4);
         let actual = extract_claim_info(claim_str);
 
         assert_eq!(actual, expected);
